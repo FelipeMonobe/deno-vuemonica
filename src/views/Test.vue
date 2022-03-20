1ee5f320ed1c09e9e897e3f832198e98a5803037
Felipe Monobe <xinube@gmail.com>
2022-03-20 16:13:05 -0300

    testing build with git hooks

---
+++ src/views/Test.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const titulo = ref("Título");
+</script>
+
+<template>
+<h1 class="h1red">{{ titulo }}</h1>
+<input type="text" v-model="titulo" />
+</template>
+
+<style scoped>
+.h1red {
+  color: red;
+}
+</style>
